1cc52405c09c818c9294f369289b7ade8d285d85
ZaraQureshi <qzara819@gmail.com>
2025-12-15 16:54:08 +0530

    add invoice schema

---
+++ src/lib/InvoiceSchema.ts
@@ -0,0 +1,20 @@
+export type InvoiceFormValues = {
+  logo?: FileList;
+  color: string;
+
+  providerName: string;
+  providerAddress: string;
+  providerPhone: string;
+  providerEmail: string;
+
+  userId: string;
+  recievedFrom: string;
+  phone: string;
+  email: string;
+  address: string;
+
+  subscriptionType: string;
+  renewalDate: string;
+  speed: string;
+  totalAmount: string;
+};
