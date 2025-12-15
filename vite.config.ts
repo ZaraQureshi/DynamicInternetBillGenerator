5eb95309f3605932705a7a4efdd9c8633305050a
ZaraQureshi <qzara819@gmail.com>
2025-12-15 17:23:46 +0530

    add type module to package,json

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite"
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+ // ESM replacement for __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  
-  // Optional: Automatically open the browser on start and set port
+  plugins: [react(), tailwindcss()],
+
   server: {
     open: true,
     port: 3000,
   },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
