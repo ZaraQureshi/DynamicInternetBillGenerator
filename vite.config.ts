98858b5a1957cd6ab972f0f248f8e54dcb7c3af4
ZaraQureshi <qzara819@gmail.com>
2025-12-23 12:25:50 +0530

    modify base

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-     base: "/DynamicInternetBillGenerator/",
+     base: "/",
   plugins: [react(), tailwindcss()],
 
   server: {
